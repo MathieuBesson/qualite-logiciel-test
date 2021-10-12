20e1e115bd81ddc761f55fb488929b6ab1789019
mathieu-besson <mathieubesson01@orange.fr>
2021-10-12 14:11:34 +0200

    Jenkins test 3

---
+++ donnee/sites/test-qualite/tests/CheckAuthenticationTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests;
+
+use App\Service\CheckAuthentication;
+use PHPUnit\Framework\TestCase;
+
+class CheckAuthenticationTest extends TestCase
+{
+
+    public function test_check_username_validity_is_string()
+    {
+        $checkAuthentication = new CheckAuthentication();
+        $this->assertEquals(true, $checkAuthentication->checkUsernameValidity('admin'));
+    }
+
+    public function test_check_username_validity_not_string()
+    {
+        $checkAuthentication = new CheckAuthentication();
+        $notStringValues = [5, [], false];
+        foreach ($notStringValues as $notStringValue) {
+            $this->assertEquals(false, $checkAuthentication->checkUsernameValidity($notStringValue));
+        }
+    }
+
+    public function test_check_username_validity_string_with_lenght_inferior_minimum()
+    {
+        $checkAuthentication = new CheckAuthentication();
+        $this->assertEquals(false, $checkAuthentication->checkUsernameValidity('a'));
+    }
+
+    public function test_check_username_validity_string_with_lenght_superior_maximum()
+    {
+        $checkAuthentication = new CheckAuthentication();
+        $this->assertEquals(false, $checkAuthentication->checkUsernameValidity('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu'));
+    }
+}
