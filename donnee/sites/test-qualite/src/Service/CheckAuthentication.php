94dddab30d385a20e67843a0c4484dc19ffa8e5a
mathieu-besson <mathieubesson01@orange.fr>
2021-10-17 19:37:34 +0200

    Test jenkins

--- donnee/sites/test-qualite/src/Service/CheckAuthentication.php
+++ donnee/sites/test-qualite/src/Service/CheckAuthentication.php
@@ -6,7 +6,7 @@ class CheckAuthentication
 {
     public function checkPasswordValidity($password): string
     {
-        return is_string($password) && preg_match("/^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{6,}$/", $password) === 1;
+        return preg_match("/^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{6,}$/", $password) === 1;
     }
 
     public function checkUsernameValidity($username): string
